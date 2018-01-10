17aed7a4c2431875ce01ac415a04b2e37ae33fd3
Inhji <johnnie@posteo.de>
2018-01-10 21:40:14 +0100

    add initial authCheck

---
+++ src/lib/checkAuth.js
@@ -0,0 +1,15 @@
+import { AsyncStorage } from 'react-native'
+
+export default async function checkAuth() {
+  try {
+    const token = await AsyncStorage.getItem('API_AUTH_TOKEN', token)
+
+    if (token !== null) {
+      return true
+    } else {
+      return false
+    }
+  } catch (e) {
+    return false
+  }
+}
